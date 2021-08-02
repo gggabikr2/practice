1ef6a1d3a928050247d0190535bb883c80c69238
gggabikr <gggabikr@gmail.com>
2021-08-02 13:12:33 -0700

    Update calling_method.js

--- calling_method.js
+++ calling_method.js
@@ -1,3 +1,4 @@
+//singleton pattern
 let counter1 = {
   value:0,
   increase: function(){
@@ -20,3 +21,37 @@ console.log(counter1.getValue())
 counter1.decrease()
 console.log(counter1.getValue())
 // console.log(counter1.value)
+
+//through the singleton pattern, you can only make one output from it.
+
+//if I want to make more than one output with one object, I do it like below.
+
+function makeCounter(){
+  return {
+    value:0,
+    increase: function(){
+      this.value++ //메소드 호출을 할경우 this는 makeCounter함수가 리턴하는 익명의 객체를 가리킨다.
+    },
+    decrease: function(){
+      this.value--
+    },
+    getValue: function(){
+      return this.value
+    }
+  }
+}
+
+let counter2 = makeCounter()
+counter2.increase()
+counter2.increase()
+counter2.decrease()
+counter2.decrease()
+console.log(counter2.getValue())
+
+let counter3 = makeCounter()
+counter3.increase()
+counter3.decrease()
+counter3.decrease()
+counter3.decrease()
+console.log(counter3.getValue())
+
