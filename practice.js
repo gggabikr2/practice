e56b1f834d88ce2c722b2eb0ee1d81ef7ad28b11
gggabikr <gggabikr@gmail.com>
2021-08-19 09:38:28 -0700

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,8 +1,62 @@
-function sumOfArraysInArray(arr) {
-    let ddd=[];
-    for (i=0; i<arr.length; i++){
-      let ccc = arr[i].filter((el)=>typeof(el)==='number').reduce((a,b)=>a+b,0);
-      ddd.push(ccc)
-    }
-    return ddd.reduce((a,b)=>a+b)
+class Counter {
+  constructor() {
+    this.value = 0;
   }
+
+  increase() {
+    this.value++;
+  }
+
+  decrease() {
+    this.value--;
+  }
+
+  getValue() {
+    return this.value;
+  }
+}
+
+let counter1 = new Counter()
+counter1.increase()
+counter1.getValue() // 1
+counter1.value // 1: 접근가능
+
+let counter2 = new Counter()
+counter2.decrease()
+counter2.decrease()
+counter2.getValue() // -2
+
+counter2.value = 4;
+
+console.log(counter2.getValue())
+console.log(counter2.value)
+
+
+// function makeCounter() {
+//   let value = 0
+//   return {
+//     increase: function() {
+//       value++ 
+//     },
+//     decrease: function() {
+//       value--
+//     },
+//     getValue: function() {
+//       return value;
+//     }
+//   }
+// }
+
+// let counter1 = makeCounter()
+// counter1.increase()
+// counter1.getValue() // 1
+
+// let counter2 = makeCounter()
+// counter2.decrease()
+// counter2.decrease()
+// counter2.getValue() // -2
+
+// counter2.value = 3
+
+// console.log(counter2.getValue())
+// console.log(counter2.value)
